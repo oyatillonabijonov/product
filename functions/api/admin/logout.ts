a2ba43b9ed849505ef5181c31a186700b1dd5f72
Oyatillo <designnabijonov@gmail.com>
2026-07-02 16:06:41 +0500

    chore: remove legacy pages functions and dead code after ssr migration

--- functions/api/admin/logout.ts
+++
@@ -1,7 +0,0 @@
-import type { Env } from '../../env';
-import { json } from '../../lib/db';
-import { clearedSessionCookie } from '../../lib/auth';
-
-export const onRequestPost: PagesFunction<Env> = async () => {
-  return json({ ok: true }, { headers: { 'set-cookie': clearedSessionCookie() } });
-};
--- functions/api/admin/me.ts
+++
@@ -1,6 +0,0 @@
-import type { Env } from '../../env';
-import { json } from '../../lib/db';
-
-export const onRequestGet: PagesFunction<Env, string, { username: string }> = async ({ data }) => {
-  return json({ username: data.username });
-};
